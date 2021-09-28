14bc463872c601dc2fdd2cbc02cdf66140f69d36
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-28 13:43:19 -0400

    Add project files.

---
+++ Bootcamp Curriculum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bootcamp_Curriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitgithub = new Topic("Git/Github","SCM", 3);
+            var sqlserver = new Topic("SQL Server", "Database", 2);
+            gitgithub.Print();
+            sqlserver.Print();
+        }
+    }
+
+}
---
+++ Bootcamp Curriculum/Topic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp_Curriculum
+{
+    class Topic
+    {
+        public static int NextId { get; set; } = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; } 
+
+        public void Print()
+        {
+            Console.WriteLine($" ID: {Id}, Name: {Name}, Category: {Category}, Difficulty: {Difficulty}");
+        }
+
+        public Topic(string Name, string Category, int Difficulty) 
+        {
+            this.Id = NextId++;
+            this.Name = Name;
+            this.Category = Category;
+            this.Difficulty = Difficulty;
+        }
+    }
+}
